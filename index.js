ca894001ade581640a9d9c2ea108802c05ce7bbd
Sabrina5962 <sabrinastievet@gmail.com>
2022-09-12 14:34:10 +0200

    Ajout du H1

---
+++ index.js
@@ -0,0 +1,11 @@
+let scorePSG = 12
+;
+let scoreRealMadrid = 12;
+
+if (scorePSG > scoreRealMadrid) {
+    console.log("Vous êtes en train de rêver il est temps d'aller au boulot")
+}else if (scorePSG < scoreRealMadrid) {
+    console.log("Bah logique ! ")
+}else {
+    console.log("Quoi ! match null!!")
+}
